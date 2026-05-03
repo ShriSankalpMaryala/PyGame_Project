e2877412d2fbb90a8c9d314d331ef2b7d43bbb2f
ShriSankalpMaryala <shivamadhuri54@gmail.com>
2026-05-03 09:48:58 +0100

    Add files via upload

--- flappy_ball.py
+++ flappy_ball.py
@@ -2,12 +2,15 @@ import pgzrun
 TITLE = "Flappy Ball Game"
 WIDTH = 600
 LENGTH = 600
+GRAVITY = 500
 class Ball():
     def __init__(self, c, r, x, y):
         self.color = c
         self.radius = r
         self.x = x
         self.y = y
+        self.vx = 40
+        self.vy = 0
     def draw(self):
         screen.draw.filled_circle((self.x,self.y), self.radius, self.color)
 
@@ -21,6 +24,25 @@ def draw():
     bouncyball.draw()
 
 
-def update():
-    pass
+def update(dt):
+    uy = bouncyball.vy
+    bouncyball.vy += GRAVITY * dt
+    bouncyball.y += (uy + bouncyball.vy) * 0.5 * dt
+    bouncyball.x += bouncyball.vx * dt
+    if bouncyball.y > 600 - bouncyball.radius:
+        bouncyball.vy *= -1
+    if bouncyball.x > 600 - bouncyball.radius:
+        bouncyball.vx *= -1
+    if bouncyball.x <bouncyball.radius:
+        bouncyball.vx *= -1
+    if bouncyball.y <bouncyball.radius:
+        bouncyball.vy *= -1
+
+
+
+
+
+
+
+
 pgzrun.go()
