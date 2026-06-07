6a500085ef3125181e6a71896eab16b37d7f64b4
ShriSankalpMaryala <shivamadhuri54@gmail.com>
2026-06-07 10:02:02 +0100

    Add files via upload

---
+++ Rocket_and_Space.py
@@ -0,0 +1,54 @@
+import pygame
+import random
+TITLE = "Rocket in Space"
+WIDTH = 600
+HEIGHT = 600
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+bg1 = pygame.image.load("rocket.png")
+bg2 = pygame.image.load("milky way.png")
+rocket_x = 300
+rocket_y = 300
+run = True
+while run == True:
+    screen.blit(bg2,(0,0))
+    screen.blit(bg1,(rocket_x,rocket_y))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                rocket_x -= 10
+            if event.key == pygame.K_w:
+                rocket_y -= 10
+            if event.key == pygame.K_s:
+                rocket_y += 10
+            if event.key == pygame.K_d:
+                rocket_x += 10
+    
+    keypressed = pygame.key.get_pressed()
+    if keypressed[pygame.K_UP]:
+        rocket_y -= 2
+    if keypressed[pygame.K_DOWN]:
+        rocket_y += 2
+    if keypressed[pygame.K_LEFT]:
+        rocket_x -= 2
+    if keypressed[pygame.K_RIGHT]:
+        rocket_x += 2
+
+    if rocket_y < 0:
+        rocket_y = 0
+
+
+    
+
+            
+            
+            
+            
+
+    pygame.display.update()
+
+
+
+
+
