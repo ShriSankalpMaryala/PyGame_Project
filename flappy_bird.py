9cea77bcda5e9427f1e56b88e0dbec815a695f73
ShriSankalpMaryala <shivamadhuri54@gmail.com>
2026-08-25 11:06:42 +0100

    Add files via upload

--- flappy_bird.py
+++ flappy_bird.py
@@ -5,7 +5,7 @@ HEIGHT = 700
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 run = True
 b1 = pygame.image.load("flappy bird.png")
-obstacle = pygame.image.load("obstacle.png")
+o = pygame.image.load("obstacle.png")
 bg = pygame.image.load("background.png")
 b2 = pygame.image.load("flappy bird 1.png")
 b3 = pygame.image.load("flappy bird 2.png")
@@ -35,8 +35,22 @@ class Flappybird(pygame.sprite.Sprite):
         if pygame.mouse.get_pressed()[0]:
             self.vy = -3
 
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self,image,x,y):
+        super().__init__()
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def update(self):
+        self.rect.x -= 1
+        if self.rect.x < -50:
+            self.kill()
 
 
+
+
+pipegroup = pygame.sprite.Group()
 
 bird = Flappybird(100,300)
 birdgroup = pygame.sprite.Group()
@@ -46,11 +60,21 @@ birdgroup.add(bird)
 
 
 groundx = 0
+pipefrequency = 5000
+lastpipe = pygame.time.get_ticks() - pipefrequency
 while run == True:
     screen.blit(bg,(0,0))
     birdgroup.draw(screen)
+    pipegroup.draw(screen)
     bird.update()
+    timenow = pygame.time.get_ticks()
+    if timenow - lastpipe > pipefrequency:
+
+        pipe = Obstacle(o,800,450)
+        pipegroup.add(pipe)
+        lastpipe = timenow
     screen.blit(g,(groundx,600))
+    pipegroup.update()
     groundx -= 1
     if groundx < -100:
         groundx = 0
